476fa59a07d74c9d24219a3cbb4f0b7ceda2cc8a
B Laureen <beffylaureen@me.com>
2023-07-05 21:19:13 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,66 @@
+
+
+
+
+var questionItems = [
+  {
+    question:  "Question #1: What does HTML stand for?", 
+    choices: ["A. Hyper Text Marking Language", "B. Hyper Text Marketing Language", "C. Hyper Text Markup Language", "D. Hyper Text Markup Leveler"], 
+    answer: "C"
+  },
+  {
+    question:  "Question #2: _____ is the process of finding errors and fixing them within a program.",
+    choices: ["A. Compiling", "B. Executing", "C. Debugging", "D. Scanning"],
+    answer: C
+  },
+  { 
+    question: "Question #3: If I say the word ALGORITHM, what am I talking about?",
+    choices: ["A. We all to to a store called Rithm", "B. Algae that can dance", "C. A new dance move", "D. A list of steps you follow to finish a task"],
+    answer: D
+  },
+  {
+    question: "Question #4: What does CSS stand for?",
+    choices: ["A. Cash Spending Secrets", "B. Cascading Style Sheets", "C. Computer Stupid System", "D. Corporate Secret Service"],
+    answer: B
+  },
+  {
+    question: "Question #5: Which keyboard symbol is used to indicate a closing tag?",
+    choices: ["A. Angle bracket", "B. Hashtag or number sign", "C. Asterisk", "D. Forward slash"],
+    answer: D
+  },
+  {
+    question: "Question #6: What tag is used to define a hyperlink, or link to another page?",
+    choices: ["A. <blockquote>", "B. <strong>", "C. <a>", "D. <em>"],
+    answer: C
+  },
+  {
+    question: "Question #7: What tag is used to define a standard cell inside a table",
+    choices: ["A. <button>", "B. <footer>", "C. <h1> to <h6>", "D. <td>"],
+    answer: D
+  },
+  {
+    question: "Question #8: In CSS and HTML, colors are displayed by combining these three shades of light:",
+    choices: ["A. Mauve, Salmon, and Black", "B. Red, Blue, and Green", "C. Yellow, Blue, and Green", "D. Violet, Red, and Orange"],
+    answer: B
+  },
+  {
+    question: "Question #9: What is the value called that defines colors such as the following: #FFFF00?",
+    choices: ["A. Color value", "B. RGB value", "C. Hex value", "D. Decimal value" ],
+    answer: C
+  },
+  {
+    question: "Question #10: What is the CSS property that offers extra information about something when you hover over an element?",
+    choices: ["A. Tooltip", "B. Tutorial", "C. Hint", "D. Info block"],
+    answer: A
+  },
+  {
+    question:  "Question #11: What is a JavaScript element that represents either TRUE or FALSE values?",
+    choices: ["A. Boolean", "B. Condition", "C. Event", "D. Do something"],
+    answer: A
+  },
+  {
+    question:  "Question #12: What can loops offer JavaScript code as a whole?",
+    choices: ["A. Added plug-ins", "B. Cross-platform support", "C. Cleaner syntax", "D. Improved performance"],
+    answer: D
+  }
+];
