da975cf2b051b8611a68af42db0ed96c417bad83
B Laureen <beffylaureen@me.com>
2023-07-09 19:16:35 -0500

    commit 07092023 1916

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,9 @@ var timeInterval;
 var questionIndex = 0;
 
 
-var questions = [
+const questions = [
   {
-    question:  "Question #1: What does HTML stand for?", 
+    question:  "What does HTML stand for?", 
     choices:[
     {text:"Hyper Text Marking Language", correct: false}, 
     {text: "Hyper Text Marketing Language", correct: false}, 
@@ -20,7 +20,7 @@ var questions = [
   },
 
   {
-    question:  "Question #2: _____ is the process of finding errors and fixing them within a program.",
+    question:  "_____ is the process of finding errors and fixing them within a program.",
     choices: [
     {text: "Compiling", correct: false},
     {text: "Executing", correct: false},
@@ -30,7 +30,7 @@ var questions = [
   
   },
   { 
-    question: "Question #3: If I say the word ALGORITHM, what am I talking about?",
+    question: "If I say the word ALGORITHM, what am I talking about?",
     choices: [
       {text: "We all to to a store called Rithm", correct: false},
       {text: "Algae that can dance", correct: false},
@@ -40,7 +40,7 @@ var questions = [
     
   },
   {
-    question: "Question #4: What does CSS stand for?",
+    question: "What does CSS stand for?",
     choices: [
       {text: "Cash Spending Secrets", correct: false},
       {text: "Cascading Style Sheets", correct: true},
@@ -50,7 +50,7 @@ var questions = [
     
   },
   {
-    question: "Question #5: Which keyboard symbol is used to indicate a closing tag?",
+    question: "Which keyboard symbol is used to indicate a closing tag?",
     choices: [
       {text: "Angle bracket", correct: false},
       {text: "Hashtag or number sign", correct: false},
@@ -60,7 +60,7 @@ var questions = [
     
   },
   {
-    question: "Question #6: What tag is used to define a hyperlink, or link to another page?",
+    question: "What tag is used to define a hyperlink, or link to another page?",
     choices: [
       {text: "<blockquote>", correct: false},
       {text: "<strong>", correct: false},
@@ -70,7 +70,7 @@ var questions = [
     
   },
   {
-    question: "Question #7: What tag is used to define a standard cell inside a table",
+    question: "What tag is used to define a standard cell inside a table",
     choices: [
       {text: "<button>", correct: false},
       {text: "<footer>", correct: false},
@@ -80,7 +80,7 @@ var questions = [
     
   },
   {
-    question: "Question #8: In CSS and HTML, colors are displayed by combining these three shades of light:",
+    question: "In CSS and HTML, colors are displayed by combining these three shades of light:",
     choices: [
       {text: "Mauve, Salmon, and Black", correct: false},
       {text: "Red, Blue, and Green", correct: true},
@@ -90,7 +90,7 @@ var questions = [
     
   },
   {
-    question: "Question #9: What is the value called that defines colors such as the following: #FFFF00?",
+    question: "What is the value called that defines colors such as the following: #FFFF00?",
     choices: [
       {text: "Color value", correct: false},
       {text: "RGB value", correct: false},
@@ -100,7 +100,7 @@ var questions = [
     
   },
   {
-    question: "Question #10: What is the CSS property that offers extra information about something when you hover over an element?",
+    question: "What is the CSS property that offers extra information about something when you hover over an element?",
     choices: [
       {text: "Tooltip", correct: true},
       {text: "Tutorial", correct: false},
@@ -110,7 +110,7 @@ var questions = [
     
   },
   {
-    question:  "Question #11: What is a JavaScript element that represents either TRUE or FALSE values?",
+    question:  "What is a JavaScript element that represents either TRUE or FALSE values?",
     choices: [
       {text: "Boolean", correct: true},
       {text: "Condition", correct: false},
@@ -120,7 +120,7 @@ var questions = [
     
   },
   {
-    question:  "Question #12: What can loops offer JavaScript code as a whole?",
+    question:  "What can loops offer JavaScript code as a whole?",
     choices: [
       {text: "Added plug-ins", correct: false},
       {text: "Cross-platform support", correct: false},
@@ -130,10 +130,59 @@ var questions = [
     
   }
 ];
-  for (let i=0; i<questionItems; i++){
-    const currItem=questionItems[i]
-  }
 
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("choices");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+  currentQuetionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion(){
+  resetState()
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNumber = currentQuestionIndex +1;
+  questionElement.innerHTML = questionNumber + ". "+ currentQuestion.question;
+  currentQuestion.choices.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+  
+}
+
+function resetState(){
+  nextButton.style.display = "none";
+  while(choices.firstChild){
+    choices.removeChild(choicesw.firstChild);
+    if(answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  } 
+}
+
+function selectAnswer(e){
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+  }else{
+    selectedBtn.classList.add("incorrect");
+    
+  }
+  }
+}
+
+startQuiz()
 
 function startQuiz(){
   // Hide the start section
