a3749a366b2a8e6f0f44df7a527acdcda74554d7
B Laureen <beffylaureen@me.com>
2023-07-23 15:41:35 -0500

    commit 07232023 1541

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,22 +1,38 @@
-var scoreList = document.querySelector('#score-list')
+var yourScore = document.getElementById('your-score')
+var initialsEl = document.querySelector ('#initials');
+var enterInitials = document.querySelector ('#enter-initials');
 
-var allScores = JSON.parse(localStorage.getItem("scores"))
+function displayHighScores(){
+  scoreList.innerHTML="";
+  var showScores = JSON.parse(localStorage.getItem('scores'))||[];
+  if (showScores.length>0){
+    getScore.forEach(function (scores){
+      var liEl=document.createElement ("li");
+      liEl.textContent=scores;
+      liEl.setAttribute("class", "scores");
+      scoreList.append(liEl);
+    })
+    
+    
+  }
+}
 
+displayHighScores();
 
-console.log('All Scores: ', allScores)
+submitInitials.addEventListener("click", function(){
+  var addInitials=document.querySelector("#enter-initials");
+  var newInitials=addInitials.value;
+  if (newInitials===""){
+    alert("Please enter your initials")
+    return
+  }
 
+  var allScores = JSON.parse(localStorage.getItem('scores'))||[]
 
+  allScores.push(scores);
+  localStorage.setItem('scores', JSON.stringify(allScores));
 
+  displayHighScores();
 
-
-// loop through all scores
-
-// dynamically create element li
-    var liEl = document.createElement('li');
-
-  // Give Li content (hint: use object dot notation!!!!)
-
-// Append to the page
-
-
-scoreList.textContent = allScores;
+  addInitials.value="";
+})
